f777b13097f8912eed0f1933fc0e2a38f842c83f
steveoh <sgourley@utah.gov>
2019-05-07 17:54:03 -0600

    docs: add driver download link

--- forklift/vault/database.template.py
+++ forklift/vault/database.template.py
@@ -5,4 +5,5 @@ database.py
 A module that holds the secrets for the database
 '''
 
+#: odbc 17 driver: https://www.microsoft.com/en-us/download/details.aspx?id=56567
 CONNECTION = 'mssql+pyodbc://(local)/corrections?driver=ODBC+Driver+17+for+SQL+Server'
