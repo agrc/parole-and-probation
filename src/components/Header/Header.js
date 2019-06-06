c46d85eccd4e6cae50f1df31e9e5c490c19d976a
steveoh <sgourley@utah.gov>
2019-06-06 11:29:15 -0600

    feat: use convention-commit to generate changelog
    refs #21

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ export default function Header(props) {
     <div className="app__header">
       <h1 className="header__heading">
         <span>{props.title}</span>
-        <a className="heading__version" href="/changelog.html" target="_blank" rel="noopener">{props.version}</a>
+        <a className="heading__version" href="https://github.com/agrc/parole-and-probation/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">{props.version}</a>
       </h1>
       {window.innerWidth >= config.MIN_DESKTOP_WIDTH && <img src={logo} className="heading__img" alt="agrc logo" />}
     </div>
