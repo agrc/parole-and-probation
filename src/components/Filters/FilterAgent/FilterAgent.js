5fb3c54c73a91c0d51305d9d502c4cd661861db1
steveoh <sgourley@utah.gov>
2019-06-12 11:13:29 -0600

    feat(filter-agent): type ahead behavior
    refs #8

--- src/components/Filters/FilterAgent/FilterAgent.js
+++ src/components/Filters/FilterAgent/FilterAgent.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 import { FormGroup, Container, Col, Button, Input, Label, InputGroup, InputGroupAddon } from 'reactstrap';
-import useInputState from '../useInputState';
+import Downshift from 'downshift'
+import useDownshiftState from '../useDownshiftState';
 import './FilterAgent.css';
+
+const testData = {
+    agents: [
+        { value: 'Andrew', supervisor: 'Sarah' },
+        { value: 'Steve', supervisor: 'Susan' },
+        { value: 'Karl', supervisor: 'Sarah' },
+        { value: 'Matt', supervisor: 'Susan' },
+        { value: 'Mitt', supervisor: 'Sarah' },
+    ],
+    supervisors: [
+        { value: 'Sarah' },
+        { value: 'Susan' },
+    ]
+};
 
 export default function FilterAgent(props) {
     const loggedInUser = 'logged in user';
     const [agents, setAgent] = useState([loggedInUser]);
-    const agent = useInputState();
-    const supervisor = useInputState();
+    const agent = useDownshiftState();
+    const supervisor = useDownshiftState();
 
     const updateAgents = (agentName, add) => {
         if (!agentName) {
@@ -18,9 +33,9 @@ export default function FilterAgent(props) {
 
         if (add) {
             updates.add(agentName);
-            agents.forEach((item) => updates.add(item));
+            agents.forEach(item => updates.add(item));
         } else {
-            agents.forEach((item) => updates.add(item));
+            agents.forEach(item => updates.add(item));
             updates.delete(agentName);
         }
 
@@ -28,14 +43,16 @@ export default function FilterAgent(props) {
         agent.reset()
     };
 
-    const addAgentsForSupervisor = (supervisorName) => {
+    const addAgentsForSupervisor = supervisorName => {
         const updates = new Set();
 
         if (vanityCheck()) {
             updates.add(loggedInUser);
         }
 
-        ['sarah', 'george', 'mike'].forEach((item) => updates.add(item));
+        testData.agents
+            .filter(agent => agent.supervisor.toLowerCase() === supervisorName.toLowerCase())
+            .forEach(item => updates.add(item.value));
 
         if (!supervisorName) {
             updates.clear();
@@ -65,46 +82,182 @@ export default function FilterAgent(props) {
                     </FormGroup>
                     <FormGroup>
                         <Label>Agent</Label>
-                        <InputGroup>
-                            <Input type="text"
-                                onChange={agent.onChange}
-                                onKeyPress={(event) => {
-                                    if (event.key !== 'Enter') {
-                                        return;
+                        <Downshift
+                            onChange={agent.onChange}
+                            stateReducer={(_, changes) => {
+                                switch (changes.type) {
+                                    case Downshift.stateChangeTypes.blurInput: {
+                                        return {
+                                            ...changes,
+                                            isOpen: false,
+                                            inputValue: agent.value
+                                        }
                                     }
-
-                                    updateAgents(agent.value, true);
-                                }}
-                                value={agent.value} />
-                            <InputGroupAddon addonType="append">
-                                <Button onClick={() => updateAgents(agent.value, true)}>Add</Button>
-                            </InputGroupAddon>
-                        </InputGroup>
+                                    case Downshift.stateChangeTypes.clickItem: {
+                                        return {
+                                            ...changes,
+                                            isOpen: false,
+                                            inputValue: changes.inputValue,
+                                            selectedItem: changes.inputValue
+                                        }
+                                    }
+                                    default: {
+                                        return changes
+                                    }
+                                }
+                            }}
+                            itemToString={item => (item ? item.value : '')}>
+                            {({
+                                getInputProps,
+                                getMenuProps,
+                                getItemProps,
+                                isOpen,
+                                clearSelection,
+                                inputValue,
+                                highlightedIndex,
+                            }) => (
+                                    <div>
+                                        <InputGroup>
+                                            <Input {...getInputProps({
+                                                onKeyDown: event => {
+                                                    switch (event.key) {
+                                                        case 'Tab': {
+                                                            const { value } = testData.agents
+                                                                .filter(item => inputValue && agents.indexOf(item.value) === -1 && item.value.toLowerCase().includes(inputValue.toLowerCase()))[0];
+                                                            agent.onChange(value);
+                                                            break;
+                                                        }
+                                                        case 'Enter': {
+                                                            updateAgents(agent.value, true);
+                                                            clearSelection();
+                                                            break;
+                                                        }
+                                                        default:
+                                                            break;
+                                                    }
+                                                }
+                                            })} />
+                                            <InputGroupAddon addonType="append">
+                                                <Button onClick={() => {
+                                                    updateAgents(agent.value, true);
+                                                    clearSelection();
+                                                }}>Add</Button>
+                                            </InputGroupAddon>
+                                        </InputGroup>
+                                        <div className="downshift__match-dropdown" {...getMenuProps()}>
+                                            <ul className="downshift__matches">
+                                                {isOpen
+                                                    ? testData.agents
+                                                        .filter(item => inputValue && agents.indexOf(item.value) === -1 && item.value.toLowerCase().includes(inputValue.toLowerCase()))
+                                                        .map((item, index) => (
+                                                            <li {...getItemProps({
+                                                                key: item.value,
+                                                                index,
+                                                                item,
+                                                                className: 'downshift__match-item' + (highlightedIndex === index ? ' downshift__match-item--selected' : '')
+                                                            })}>
+                                                                {item.value}
+                                                            </li>
+                                                        ))
+                                                    : null}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                )}
+                        </Downshift>
                     </FormGroup>
                     <FormGroup>
                         <Label>Supervisor</Label>
-                        <InputGroup>
-                            <Input type="text"
-                                onChange={supervisor.onChange}
-                                onKeyPress={(event) => {
-                                    if (event.key !== 'Enter') {
-                                        return;
+                        <Downshift
+                            onChange={supervisor.onChange}
+                            stateReducer={(_, changes) => {
+                                switch (changes.type) {
+                                    case Downshift.stateChangeTypes.blurInput: {
+                                        return {
+                                            ...changes,
+                                            isOpen: false,
+                                            inputValue: supervisor.value
+                                        }
                                     }
-
-                                    addAgentsForSupervisor(supervisor.value);
-                                }}
-                                value={supervisor.value} />
-                            <InputGroupAddon addonType="append">
-                                <Button onClick={() => addAgentsForSupervisor(supervisor.value)}>Set</Button>
-                            </InputGroupAddon>
-                        </InputGroup>
+                                    case Downshift.stateChangeTypes.clickItem: {
+                                        return {
+                                            ...changes,
+                                            isOpen: false,
+                                            inputValue: changes.inputValue,
+                                            selectedItem: changes.inputValue
+                                        }
+                                    }
+                                    default: {
+                                        return changes
+                                    }
+                                }
+                            }}
+                            itemToString={item => (item ? item.value : '')}>
+                            {({
+                                getInputProps,
+                                getMenuProps,
+                                getItemProps,
+                                isOpen,
+                                clearSelection,
+                                inputValue,
+                                highlightedIndex,
+                            }) => (
+                                    <div>
+                                        <InputGroup>
+                                            <Input {...getInputProps({
+                                                onKeyDown: event => {
+                                                    switch (event.key) {
+                                                        case 'Tab': {
+                                                            const { value } = testData.supervisors
+                                                                .filter(item => inputValue && item.value.toLowerCase().includes(inputValue.toLowerCase()))[0];
+                                                            supervisor.onChange(value);
+                                                            break;
+                                                        }
+                                                        case 'Enter': {
+                                                            addAgentsForSupervisor(supervisor.value);
+                                                            clearSelection();
+                                                            break;
+                                                        }
+                                                        default:
+                                                            break;
+                                                    }
+                                                }
+                                            })} />
+                                            <InputGroupAddon addonType="append">
+                                                <Button onClick={() => {
+                                                    addAgentsForSupervisor(supervisor.value);
+                                                    clearSelection();
+                                                }}>Set</Button>
+                                            </InputGroupAddon>
+                                        </InputGroup>
+                                        <div className="downshift__match-dropdown" {...getMenuProps()}>
+                                            <ul className="downshift__matches">
+                                                {isOpen
+                                                    ? testData.supervisors
+                                                        .filter(item => !inputValue || item.value.toLowerCase().includes(inputValue.toLowerCase()))
+                                                        .map((item, index) => (
+                                                            <li {...getItemProps({
+                                                                key: item.value,
+                                                                index,
+                                                                item,
+                                                                className: 'downshift__match-item' + (highlightedIndex === index ? ' downshift__match-item--selected' : '')
+                                                            })}>
+                                                                {item.value}
+                                                            </li>
+                                                        ))
+                                                    : null}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                )}
+                        </Downshift>
                     </FormGroup>
                 </Col>
                 <Col>
                     <FormGroup>
                         <Label>Agent List</Label>
                         <Input type="select" multiple readOnly
-                            onDoubleClick={(event) => updateAgents(event.target.value, false)}>
+                            onDoubleClick={event => updateAgents(event.target.value, false)}>
                             {agents.map((agent, key) => (
                                 <option key={key}>{agent}</option>
                             ))}
