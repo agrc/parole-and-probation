6bec0d822fb62a3de3a49ae704cd5c5e440becef
steveoh <sgourley@utah.gov>
2019-06-12 17:19:47 -0600

    feat(filter-location): add address geolocation zooming

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -8,7 +8,7 @@ import FilterLocation from './FilterLocation';
 import FilterOther from './FilterOther';
 import './Filters.css';
 
-export default function Filters() {
+export default function Filters(props) {
     function reducer(_, action) {
         switch (action.type) {
             case 'selection':
@@ -26,7 +26,7 @@ export default function Filters() {
             <FilterAgent active={filters.active} />
             <FilterDate active={filters.active} />
             <FilterOffender active={filters.active} />
-            <FilterLocation active={filters.active} />
+            <FilterLocation active={filters.active} dispatcher={props.mapDispatcher} />
             <FilterOther active={filters.active} />
             <FilterActions />
         </>
