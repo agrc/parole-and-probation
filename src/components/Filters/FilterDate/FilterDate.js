3d5f5c5e678eec3c72861b106678ca2a177cfe86
steveoh <sgourley@utah.gov>
2019-06-17 14:29:18 -0600

    feat(filter): use accordion instead of selector
    closes #27

--- src/components/Filters/FilterDate/FilterDate.js
+++ src/components/Filters/FilterDate/FilterDate.js
@@ -3,7 +3,7 @@ import { Button, ButtonGroup, Container, Col, Collapse, Label, FormGroup } from 
 import Calendar from 'react-calendar'
 import './FilterDate.css';
 
-export default function FilterDate(props) {
+export default function FilterDate() {
     const [dateAttempted, setDateAttempted] = useState();
     const [dateSuccessful, setDateSuccessful] = useState();
     const [dateOffice, setDateOffice] = useState();
@@ -13,69 +13,67 @@ export default function FilterDate(props) {
     const [visit, setVisit] = useState(false);
 
     return (
-        props.active === 'Date' ?
-            <Container fluid className="filter-date">
-                <Col>
-                    <FormGroup>
-                        <Label>No Field Visit</Label>
-                        <div className="text-center">
-                            <ButtonGroup>
-                                {['No Visits'].map((binary, index) =>
-                                    <Button
-                                        key={index}
-                                        size="sm"
-                                        color={visit ? 'warning' : 'secondary'}
-                                        onClick={() => { setVisit(!visit) }}>
-                                        {binary}
-                                    </Button>
-                                )}
-                            </ButtonGroup>
-                        </div>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label onClick={() => toggleAttempted(!attempted)}>Last Attempted Field Contact</Label>
-                        <Collapse isOpen={attempted}>
-                            <Calendar
-                                selectRange
-                                maxDetail="month"
-                                minDetail="year"
-                                onChange={setDateAttempted}
-                                value={dateAttempted}
-                            />
-                        </Collapse>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label onClick={() => toggleSuccessful(!successful)}>Last Successful Field Contact</Label>
-                        <Collapse isOpen={successful}>
-                            <Calendar
-                                selectRange
-                                maxDetail="month"
-                                minDetail="year"
-                                onChange={setDateSuccessful}
-                                value={dateSuccessful}
-                            />
-                        </Collapse>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label onClick={() => toggleOffice(!office)}>Last Office Contact</Label>
-                        <Collapse isOpen={office}>
-                            <Calendar
-                                selectRange
-                                maxDetail="month"
-                                minDetail="year"
-                                onChange={setDateOffice}
-                                value={dateOffice}
-                            />
-                        </Collapse>
-                    </FormGroup>
-                </Col>
-            </Container>
-            : null
+        <Container fluid className="filter-date">
+            <Col>
+                <FormGroup>
+                    <Label>No Field Visit</Label>
+                    <div className="text-center">
+                        <ButtonGroup>
+                            {['No Visits'].map((binary, index) =>
+                                <Button
+                                    key={index}
+                                    size="sm"
+                                    color={visit ? 'warning' : 'secondary'}
+                                    onClick={() => { setVisit(!visit) }}>
+                                    {binary}
+                                </Button>
+                            )}
+                        </ButtonGroup>
+                    </div>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label onClick={() => toggleAttempted(!attempted)}>Last Attempted Field Contact</Label>
+                    <Collapse isOpen={attempted}>
+                        <Calendar
+                            selectRange
+                            maxDetail="month"
+                            minDetail="year"
+                            onChange={setDateAttempted}
+                            value={dateAttempted}
+                        />
+                    </Collapse>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label onClick={() => toggleSuccessful(!successful)}>Last Successful Field Contact</Label>
+                    <Collapse isOpen={successful}>
+                        <Calendar
+                            selectRange
+                            maxDetail="month"
+                            minDetail="year"
+                            onChange={setDateSuccessful}
+                            value={dateSuccessful}
+                        />
+                    </Collapse>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label onClick={() => toggleOffice(!office)}>Last Office Contact</Label>
+                    <Collapse isOpen={office}>
+                        <Calendar
+                            selectRange
+                            maxDetail="month"
+                            minDetail="year"
+                            onChange={setDateOffice}
+                            value={dateOffice}
+                        />
+                    </Collapse>
+                </FormGroup>
+            </Col>
+        </Container>
     )
 }
--- src/components/Filters/FilterOffender/FilterOffender.js
+++ src/components/Filters/FilterOffender/FilterOffender.js
@@ -2,61 +2,59 @@ import React, { useState } from 'react';
 import { Button, ButtonGroup, FormGroup, Label, Input, Container, Col } from 'reactstrap'
 import './FilterOffender.css';
 
-export default function FilterOffender(props) {
+export default function FilterOffender() {
     const [active, setActive] = useState();
 
     return (
-        props.active === 'Offender' ?
-            <Container fluid className="filter-offender">
-                <Col>
-                    <FormGroup>
-                        <Label>Name</Label>
-                        <Input type="text" name="name" id="name" />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Offender Number</Label>
-                        <Input type="number" name="number" id="number" />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Gender</Label>
-                        <div className="text-center">
-                            <ButtonGroup>
-                                {['Male', 'Female'].map((gender, index) =>
-                                    <Button
-                                        key={index}
-                                        size="sm"
-                                        color={active === gender ? 'warning' : 'secondary'}
-                                        onClick={() => {
-                                            if (active === gender) {
-                                                gender = null;
-                                            }
+        <Container fluid className="filter-offender">
+            <Col>
+                <FormGroup>
+                    <Label>Name</Label>
+                    <Input type="text" name="name" id="name" />
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Offender Number</Label>
+                    <Input type="number" name="number" id="number" />
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Gender</Label>
+                    <div className="text-center">
+                        <ButtonGroup>
+                            {['Male', 'Female'].map((gender, index) =>
+                                <Button
+                                    key={index}
+                                    size="sm"
+                                    color={active === gender ? 'warning' : 'secondary'}
+                                    onClick={() => {
+                                        if (active === gender) {
+                                            gender = null;
+                                        }
 
-                                            setActive(gender);
-                                        }}>
-                                        {gender}
-                                    </Button>
-                                )}
-                            </ButtonGroup>
-                        </div>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Phone</Label>
-                        <Input type="tel" name="phone" id="phone" />
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup>
-                        <Label>Employer</Label>
-                        <Input type="text" name="employer" id="employer" />
-                    </FormGroup>
-                </Col>
-            </Container>
-            : null
+                                        setActive(gender);
+                                    }}>
+                                    {gender}
+                                </Button>
+                            )}
+                        </ButtonGroup>
+                    </div>
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Phone</Label>
+                    <Input type="tel" name="phone" id="phone" />
+                </FormGroup>
+            </Col>
+            <Col>
+                <FormGroup>
+                    <Label>Employer</Label>
+                    <Input type="text" name="employer" id="employer" />
+                </FormGroup>
+            </Col>
+        </Container>
     )
 }
