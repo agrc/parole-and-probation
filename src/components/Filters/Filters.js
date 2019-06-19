fd7db6aeda189017c417946ca9fe1b9ee5bc8435
steveoh <sgourley@utah.gov>
2019-06-19 11:08:48 -0600

    fix(filters): move reducer out of funcion
    otherwise it gets recreated every render

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -22,63 +22,64 @@ const testData = {
     ]
 };
 
-export default function Filters(props) {
-    const loggedInUser = 'logged in user';
-    const vanityCheck = (agentList) => {
-        if (Array.isArray(agentList)) {
-            return agentList.indexOf(loggedInUser) > -1;
-        }
+const loggedInUser = 'logged in user';
 
-        return agentList.has(loggedInUser);
+const vanityCheck = (agentList) => {
+    if (Array.isArray(agentList)) {
+        return agentList.indexOf(loggedInUser) > -1;
     }
 
-    const reducer = (_, data) => {
-        switch (data.filter) {
-            case 'agent': {
-                console.log(`reducing state for ${data.filter}`);
-                console.dir(data);
+    return agentList.has(loggedInUser);
+};
 
-                const updates = new Set();
+const reducer = (state, data) => {
+    switch (data.filter) {
+        case 'agent': {
+            console.log(`reducing state for ${data.filter}`);
+            console.dir(data);
 
-                if (data.type === 'agent') {
-                    if (data.add) {
-                        updates.add(data.agentName);
-                        _.agent.agentList.forEach(item => updates.add(item));
-                    } else {
-                        _.agent.agentList.forEach(item => updates.add(item));
-                        updates.delete(data.agentName);
-                    }
-                } else if (data.type === 'supervisor') {
-                    if (vanityCheck(_.agent.agentList)) {
+            const updates = new Set();
+
+            if (data.type === 'agent') {
+                if (data.add) {
+                    updates.add(data.agentName);
+                    state.agent.agentList.forEach(item => updates.add(item));
+                } else {
+                    state.agent.agentList.forEach(item => updates.add(item));
+                    updates.delete(data.agentName);
+                }
+            } else if (data.type === 'supervisor') {
+                if (vanityCheck(state.agent.agentList)) {
+                    updates.add(loggedInUser);
+                }
+
+                if (!data.supervisorName) {
+                    updates.clear();
+
+                    if (vanityCheck(state.agent.agentList)) {
                         updates.add(loggedInUser);
                     }
-
-                    if (!data.supervisorName) {
-                        updates.clear();
-
-                        if (vanityCheck(_.agent.agentList)) {
-                            updates.add(loggedInUser);
-                        }
-                    } else {
-                        testData.agents
-                            .filter(agent => agent.supervisor.toLowerCase() === data.supervisorName.toLowerCase())
-                            .forEach(item => updates.add(item.value));
-                    }
-                }
-
-                return {
-                    agent: {
-                        ..._.agent,
-                        agentList: Array.from(updates),
-                        vanity: vanityCheck(updates)
-                    }
+                } else {
+                    testData.agents
+                        .filter(agent => agent.supervisor.toLowerCase() === data.supervisorName.toLowerCase())
+                        .forEach(item => updates.add(item.value));
                 }
             }
-            default:
-                throw new Error();
-        }
-    };
 
+            return {
+                agent: {
+                    ...state.agent,
+                    agentList: Array.from(updates),
+                    vanity: vanityCheck(updates)
+                }
+            }
+        }
+        default:
+            throw new Error();
+    }
+};
+
+export default function Filters(props) {
     const [criteria, dispatcher] = useReducer(reducer, {
         agent: {
             loggedInUser,
