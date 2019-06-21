08bb3c4030ba76562854102bb03a10cb3f233e99
steveoh <sgourley@utah.gov>
2019-06-20 18:05:08 -0600

    feat(filters): reset sets state to default

--- src/components/Filters/FilterActions/FilterActions.js
+++ src/components/Filters/FilterActions/FilterActions.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Col, Button } from 'reactstrap'
 import './FilterActions.css';
 
-export default function FilterActions() {
+export default function FilterActions(props) {
     return (
         <Col className="filter-actions filter-actions--space-between">
-            <Button size="lg" color="primary">Apply</Button>
-            <Button size="lg" color="secondary">Reset</Button>
+            <Button size="lg" color="secondary" onClick={props.reset}>Reset</Button>
         </Col>
     )
 }
