12e32905c81ac6b1726e046d8b0678e8404a0a0a
steveoh <sgourley@utah.gov>
2019-07-08 19:40:20 -0600

    feat(app): add react router and oidc client

--- src/ClientApp/src/config.js
+++ src/ClientApp/src/config.js
@@ -5,4 +5,13 @@ module.exports = {
     MARKER_FILL_COLOR: [130, 65, 47, 0.5],
     MARKER_OUTLINE_COLOR: [230, 126, 21, 0.7],
   },
+  oidcConfig: {
+    authority: 'https://login.dts.utah.gov/sso/oauth2/',
+    client_id: 'synange-feoffor-673742',
+    redirect_uri: process.env.REACT_APP_REDIRECT_URL,
+    response_type: 'token id_token',
+    scope: 'openid profile email',
+    filterProtocolClaims: true,
+    loadUserInfo: true
+  }
 }
--- src/ClientApp/src/index.js
+++ src/ClientApp/src/index.js
@@ -4,8 +4,32 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { makeAuthenticator, makeUserManager, Callback } from 'react-oidc';
+import { oidcConfig } from './config'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const userManager = makeUserManager(oidcConfig);
+const AppWithAuth = makeAuthenticator({
+    userManager: userManager,
+    signInArgs: {}
+})(<App />);
+
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route
+                path="/callback"
+                render={routeProps => (
+                    <Callback
+                        onSuccess={() => routeProps.history.push('/')}
+                        userManager={userManager}
+                    />
+                )}
+            />
+            <AppWithAuth />
+        </Switch>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
