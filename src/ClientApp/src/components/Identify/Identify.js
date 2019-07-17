5ab572f5a0a51d7dbfa211ea97cc3d015900e849
steveoh <sgourley@utah.gov>
2019-07-17 12:02:58 -0600

    fix(identify): move gang below crime
    meeting 7/8
    
    refs #5

--- src/ClientApp/src/components/Identify/Identify.js
+++ src/ClientApp/src/components/Identify/Identify.js
@@ -23,9 +23,6 @@ const IdentifyInformation = (props) => {
           <img alt="200x200"
             src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b6823d4d9%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b6823d4d9%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.65%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
             data-holder-rendered="true" />
-          <figcaption className="identify__figure-caption text-truncate">
-            <small className="pl-2 text-lowercase">{props.offender.gang}</small>
-          </figcaption>
           <div className="d-flex border border-left-0 identify__image-addon f-column text-center">
             <div className="d-flex pl-1 border-bottom border-info identify__addon-item">{props.offender.age} {props.offender.gender}</div>
             <div className="d-flex pl-1 border-bottom border-info identify__addon-item">{props.status.sos}</div>
@@ -37,8 +34,8 @@ const IdentifyInformation = (props) => {
       </figure>
       <h4>{props.offender.name}</h4>
       <Row className="border-bottom">
-        <Label className={`pt-0 pb-0 pr-0 font-weight-bolder text-right col-form-label col-sm-${labelSize}`}>Number</Label>
-        <Col className={`pl-0  col-sm-${12 - labelSize}`}>
+        <Label className={`py-0 pr-0 font-weight-bolder text-right col-form-label col-sm-${labelSize}`}>Number</Label>
+        <Col className={`pl-0 col-sm-${12 - labelSize}`}>
           <Label className={'pt-0 pb-0 pl-3 col-form-label'}>{props.offender.number}</Label>
         </Col>
         <Label className={`pr-0 font-weight-bolder text-right col-form-label col-sm-${labelSize}`}>Agent</Label>
@@ -107,6 +104,22 @@ const IdentifyInformation = (props) => {
           <p className={'pl-3 text-justify'}>{props.status.crime}</p>
         </Col>
       </Row>
+      {
+        props.offender.gangGroup ?
+          <>
+            <h5 className="mt-2">Gang</h5>
+            <Row className="border-bottom">
+              <Label className={`pb-0 pr-0 font-weight-bolder text-right col-form-label col-sm-${labelSize}`}>Group</Label>
+              <Col className={`pl-0 mb-1 col-sm-${12 - labelSize}`}>
+                <Label className={'pb-0 pl-3 col-form-label'}>{props.offender.gangGroup}</Label>
+              </Col>
+              <Label className={`pb-0 pr-0 font-weight-bolder text-right col-form-label col-sm-${labelSize}`}>Name</Label>
+              <Col className={`pl-0 mb-1 col-sm-${12 - labelSize}`}>
+                <Label className={'pl-3 col-form-label'}>{props.offender.gang}</Label>
+              </Col>
+            </Row>
+          </> : null
+      }
       <Row className="mt-3 j-between">
         <Col>
           <Label className="font-weight-bolder">Supervision Start</Label>
