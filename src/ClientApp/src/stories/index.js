da76c8a61aeed1b74a1c22d045285a696db47376
steveoh <sgourley@utah.gov>
2019-07-17 13:42:51 -0600

    tests(identify): add story for partial address and no phone

--- src/ClientApp/src/stories/index.js
+++ src/ClientApp/src/stories/index.js
@@ -19,9 +19,50 @@ storiesOf('Identify', module)
           gang: 'barrio mexicanos locos'
         },
         contact: {
-          phone: 'CELL: (801) 888-4325',
-          street: '123 Red Street',
+          phone: '(801) 888-4325',
+          street: '326 east south temple',
           unit: '#7',
+          city: 'Salt Lake City',
+          zip: 84111,
+          addressType: '1st physical',
+          addressDuration: new Date('December 17, 1995'),
+          employer: 'McDonalds'
+        },
+        visit: {
+          fieldDate: new Date('June 17, 2019'),
+          fieldResult: 'Successful',
+          officeDate: new Date('May 05, 2019')
+        },
+        status: {
+          legal: 'Probation',
+          warrant: 'No Active Warrant',
+          sos: 'LOW',
+          specialSupervisionTitle: 'SO',
+          specialSupervision: 'Sex Offender',
+          crimeDegree: 'F1',
+          crime: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          supervisionStart: new Date('January 1, 2018'),
+          complianceCredit: new Date('May 15, 2019')
+        }
+      }}></IdentifyInformation>
+    </IdentifyContainer>
+  ))
+  .add('with partial address and no phone', () => (
+    <IdentifyContainer>
+      <IdentifyInformation {...{
+        offender: {
+          name: 'Sarah Sanders',
+          number: 1234556,
+          age: 35,
+          gender: 'Female',
+          agent: 'Ace Ventura',
+          gangGroup: 'sureno',
+          gang: 'barrio mexicanos locos'
+        },
+        contact: {
+          phone: '',
+          street: '326 east south temple',
+          unit: '',
           city: 'Salt Lake City',
           zip: 84111,
           addressType: '1st physical',
@@ -60,8 +101,8 @@ storiesOf('Identify', module)
           gang: 'barrio mexicanos locos'
         },
         contact: {
-          phone: 'CELL: (801) 888-4325',
-          street: '123 Red Street',
+          phone: '(801) 888-4325',
+          street: '326 east south temple',
           unit: '#7',
           city: 'Salt Lake City',
           zip: 84111,
@@ -101,8 +142,8 @@ storiesOf('Identify', module)
           gang: 'barrio mexicanos locos'
         },
         contact: {
-          phone: 'CELL: (801) 888-4325',
-          street: '123 Red Street',
+          phone: '(801) 888-4325',
+          street: '326 east south temple',
           unit: '#7',
           city: 'Salt Lake City',
           zip: 84111,
@@ -143,8 +184,8 @@ storiesOf('Identify', module)
           gang: ''
         },
         contact: {
-          phone: 'CELL: (801) 888-4325',
-          street: '123 Red Street',
+          phone: '(801) 888-4325',
+          street: '326 east south temple',
           unit: '#7',
           city: 'Salt Lake City',
           zip: 84111,
