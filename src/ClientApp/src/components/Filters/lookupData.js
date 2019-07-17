56debce9052933bcc1f694397af6116596806c66
steveoh <sgourley@utah.gov>
2019-07-16 19:39:53 -0600

    refactor(filters): move agents and supervisors to lookup

--- src/ClientApp/src/components/Filters/lookupData.js
+++ src/ClientApp/src/components/Filters/lookupData.js
@@ -1,4 +1,18 @@
+const agents = [
+    { value: 'Steve', supervisor: 'Susan' },
+    { value: 'Karl', supervisor: 'Sarah' },
+    { value: 'Matt', supervisor: 'Susan' },
+    { value: 'Mitt', supervisor: 'Sarah' }
+];
+
+const supervisors = [
+    { value: 'Sarah' },
+    { value: 'Susan' },
+];
+
 const supervisionContactDays = [30, 60, 90, 180];
 export {
+    agents,
+    supervisors,
     supervisionContactDays
 };
