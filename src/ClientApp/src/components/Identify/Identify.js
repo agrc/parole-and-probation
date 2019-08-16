19e4eac8e83a2a1c1a908a3c201ee9e0a4fd4842
steveoh <sgourley@utah.gov>
2019-08-15 20:19:14 -0600

    refactor(identify): switch to css grid
    closes #47

--- src/ClientApp/src/components/Identify/Identify.js
+++ src/ClientApp/src/components/Identify/Identify.js
@@ -49,29 +49,37 @@ const IdentifyInformation = props => {
     props.offender ?
       <Container className="identify pt-4">
         {props.features.length > 1 ? (
-          <Pagination>
-            {props.features.map((_, i) =>
-              (
-                <PaginationItem active={props.index === i} key={i + 1}>
-                  <PaginationLink onClick={() => props.update({ type: 'IDENTIFY_PAGINATE', payload: i })}>{i + 1}</PaginationLink>
-                </PaginationItem>
-              )
-            )}
+          <Pagination listClassName="justify-content-center">
+            <PaginationItem disabled={props.index - 1 < 0} className="justify-content-center">
+              <PaginationLink first disabled={props.index - 1 < 0} onClick={() => props.update({ type: 'IDENTIFY_PAGINATE', payload: 0 })} />
+            </PaginationItem>
+            <PaginationItem disabled={props.index - 1 < 0}>
+              <PaginationLink previous disabled={props.index - 1 < 0} onClick={() => props.update({ type: 'IDENTIFY_PAGINATE', payload: props.index - 1 })} />
+            </PaginationItem>
+            <PaginationItem disabled>
+              <PaginationLink disabled>{`${props.index + 1} of ${props.features.length}`}</PaginationLink>
+            </PaginationItem>
+            <PaginationItem disabled={props.index + 1 >= props.features.length}>
+              <PaginationLink next disabled={props.index + 1 >= props.features.length} onClick={() => props.update({ type: 'IDENTIFY_PAGINATE', payload: props.index + 1 })} />
+            </PaginationItem>
+            <PaginationItem disabled={props.index + 1 >= props.features.length}>
+              <PaginationLink last disabled={props.index + 1 >= props.features.length} onClick={() => props.update({ type: 'IDENTIFY_PAGINATE', payload: props.features.length - 1 })} />
+            </PaginationItem>
           </Pagination>
         ) : null}
-        <figure className="mb-1 pt-1 figured-flex identify__figure identify--center-content">
-          <div className="d-flex f-row border border-info">
+        <div style={{ display: 'grid', gridTemplateColumns: '200px 1fr' }} className="mb-3 border border-info">
+          <div>
             <img alt="200x200"
               src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b6823d4d9%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b6823d4d9%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.421875%22%20y%3D%22104.65%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
               data-holder-rendered="true" />
-            <div className="d-flex border border-left-0 identify__image-addon f-column text-center">
-              <div className="d-flex pl-1 border-bottom border-info identify__addon-item">{calculateAge(extra.date_of_birth)} {props.offender.gender}</div>
-              <div className="d-flex pl-1 border-bottom border-info identify__addon-item">{props.offender.standard_of_supervision ? props.offender.standard_of_supervision : 'NO STD'}</div>
-              <div className="d-flex pl-1 border-bottom border-info identify__addon-item">{props.offender.legal_status}</div>
-              <div className={`d-flex pl-1 identify__addon-item${props.offender.active_warrant ? ' identify__addon-item--danger' : ''}`}>{props.offender.active_warrant ? 'active warrant' : 'no active warrant'}</div>
-            </div>
           </div>
-        </figure>
+          <div style={{ display: 'grid', gridTemplateRows: '1fr 1fr 1fr 1fr' }}>
+            <div className="d-flex identify__addon-item pl-1 border-bottom border-info">{calculateAge(extra.date_of_birth)} {props.offender.gender} {extra.race ? extra.race.toLowerCase() : null}</div>
+            <div className="d-flex identify__addon-item pl-1 border-bottom border-info">{props.offender.standard_of_supervision ? props.offender.standard_of_supervision : 'NO STD'}</div>
+            <div className="d-flex identify__addon-item pl-1 border-bottom border-info">{props.offender.legal_status.toLowerCase()}</div>
+            <div className={`d-flex identify__addon-item pl-1${props.offender.active_warrant ? ' identify__addon-item--danger' : ''}`}>{props.offender.active_warrant ? 'active warrant' : 'no active warrant'}</div>
+          </div>
+        </div>
         <h4>{props.offender.offender}</h4>
         <div className="identify-grid--label-text identify__row border-bottom">
           <label className="identify-grid--label-text__label font-weight-bolder text-right">Number</label>
@@ -156,7 +164,7 @@ const IdentifyInformation = props => {
         {extra.special_supervision && extra.special_supervision.length > 0 ? <>
           <h5 className="mt-2">Special Supervisions</h5>
           <div className="identify-grid--contacts identify__row identify__items-container px-3 border-bottom">
-            {extra.special_supervision.split(',').map(item => <label key={item}>{item}</label>)}
+            {extra.special_supervision.split(',').map((item, i) => <label key={item + i}>{item}</label>)}
           </div>
         </> : null}
         <h5 className="mt-2">Primary Offense</h5>
