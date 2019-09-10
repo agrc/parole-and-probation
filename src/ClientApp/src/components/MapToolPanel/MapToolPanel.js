d585c49a3d6481ed58041928f089fe361c5b6b29
steveoh <sgourley@utah.gov>
2019-09-10 11:26:30 -0600

    feat(map-tool): create map tool pane
    refs #79

---
+++ src/ClientApp/src/components/MapToolPanel/MapToolPanel.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import './MapToolPanel.css';
+
+export default function MapToolPanel(props) {
+    const [expanded, setExpanded] = useState(props.expanded || false);
+    const classes = classNames(
+        props.className,
+        'map-tool-panel',
+        'esri-widget--button',
+        'esri-widget',
+        expanded ? 'map-tool-panel--open' : false
+    );
+
+    const popOutClasses = classNames(
+        expanded ? false : 'd-none',
+        'map-tool-panel'
+    );
+
+    const iconClasses = classNames(
+        expanded ? 'd-none' : false,
+        'esri-icon'
+    );
+
+    return (
+        <div
+            className={classes}
+            role="button"
+            aria-label="Zoom to address"
+            title="Geocode"
+            onMouseOver={() => setExpanded(true)}
+            onMouseOut={() => setExpanded(false)}>
+            <FontAwesomeIcon icon={props.icon} className={iconClasses} />
+            <form className={popOutClasses}>
+                {props.children}
+            </form>
+        </div>
+    );
+};
