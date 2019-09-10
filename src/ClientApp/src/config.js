1f8f5eab0a1fbe1a307dd49ec3b43714e327580c
steveoh <sgourley@utah.gov>
2019-09-10 09:34:19 -0600

    chore: remove unused props

--- src/ClientApp/src/config.js
+++ src/ClientApp/src/config.js
@@ -2,8 +2,6 @@ module.exports = {
   mappingConfig: {
     MIN_DESKTOP_WIDTH: 768,
     WEB_MERCATOR_WKID: 3857,
-    MARKER_FILL_COLOR: [130, 65, 47, 0.5],
-    MARKER_OUTLINE_COLOR: [230, 126, 21, 0.7],
   }, oidcConfig: {
     authority: 'https://login.dts.utah.gov/sso/oauth2/',
     client_id: 'synange-feoffor-673742',
