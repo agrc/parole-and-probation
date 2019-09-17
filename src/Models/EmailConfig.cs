b36222735c687d791150f253466dbc4ffaf20f0c
steveoh <sgourley@utah.gov>
2019-09-16 19:25:57 -0600

    feat(api): send email with csv

---
+++ src/Models/EmailConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace app.Models {
+    public class EmailConfig {
+        public string Smtp { get; set; }
+        public string Pickup { get; set; }
+        public bool Testing { get; set; }
+    }
+}
