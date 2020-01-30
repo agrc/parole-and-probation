d270e92cce2cc79da750c07bbb96844130d74132
steveoh <sgourley@utah.gov>
2020-01-29 18:46:08 -0700

    tests: migrate to new storybook config

--- src/ClientApp/.storybook/config.js
+++ src/ClientApp/.storybook/config.js
@@ -1,20 +1,2 @@
-import { configure } from '@storybook/react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../src/App.css';
-
-let req;
-if (process.env.NODE_ENV === 'test') {
-  // fix for when running in jest tests
-  const registerRequireContextHook = require('babel-plugin-require-context-hook/register');
-  registerRequireContextHook();
-
-  req = global.__requireContext(__dirname, '../src', true, /\.stories\.js$/);
-} else {
-  req = require.context('../src', true, /\.stories\.js$/);
-}
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
--- src/ClientApp/.storybook/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (options) {
-  var config = options.config
-  config.plugins = config.plugins.filter(
-    p => String(p.resourceRegExp) !== "/core-js/"
-  );
-  return config;
-}
