dcae4925ccdc5468c1b257265ff8068f5cb2de24
steveoh <sgourley@utah.gov>
2020-01-30 17:42:50 -0700

    feat(map-tool-panel): show close button on mobile
    closes #92

--- src/ClientApp/src/components/MapToolPanel/MapToolPanel.js
+++ src/ClientApp/src/components/MapToolPanel/MapToolPanel.js
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
+import CloseButton from '../CloseButton';
 import './MapToolPanel.css';
 
 export default function MapToolPanel(props) {
-    const [expanded, setExpanded] = useState(props.expanded || false);
-    const classes = classNames(
-        props.className,
-        'map-tool-panel',
-        'esri-widget--button',
-        'esri-widget',
-        expanded ? 'map-tool-panel--open' : false
-    );
+  const [expanded, setExpanded] = useState(props.expanded || false);
+  const classes = classNames(
+    props.className,
+    'map-tool-panel',
+    'esri-widget--button',
+    'esri-widget',
+    expanded ? 'map-tool-panel--open' : false
+  );
 
-    const popOutClasses = classNames(
-        expanded ? false : 'd-none',
-        'map-tool-panel'
-    );
+  const popOutClasses = classNames(
+    expanded ? false : 'd-none',
+    'map-tool-panel'
+  );
 
-    const iconClasses = classNames(
-        expanded ? 'd-none' : false,
-        'esri-icon'
-    );
+  const iconClasses = classNames(
+    expanded ? 'd-none' : false,
+    'esri-icon'
+  );
 
-    return (
-        <div
-            className={classes}
-            role="button"
-            aria-label="Zoom to address"
-            title="Geocode"
-            onMouseOver={() => setExpanded(true)}
-            onMouseOut={() => setExpanded(false)}>
-            <FontAwesomeIcon icon={props.icon} className={iconClasses} />
-            <form className={popOutClasses}>
-                {props.children}
-            </form>
-        </div>
-    );
+  return (
+    <div
+      className={classes}
+      role="button"
+      aria-label="Zoom to address"
+      title="Geocode"
+      onMouseOver={() => setExpanded(true)}
+      onMouseOut={() => setExpanded(false)}>
+      <FontAwesomeIcon icon={props.icon} className={iconClasses} />
+      <form className={popOutClasses}>
+        <CloseButton className="close-button--hide-on-lg" onClick={() => setExpanded(false)} />
+        {props.children}
+      </form>
+    </div>
+  );
 };
