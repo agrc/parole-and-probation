b72b758025bda7e9c37232efbba68ba863abb70a
steveoh <sgourley@utah.gov>
2020-02-03 15:01:44 -0700

    feat(legend): create a static legend component

---
+++ src/ClientApp/src/components/StaticLegend/StaticLegend.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+import './StaticLegend.css';
+
+export default function StaticLegend(props) {
+  const breaks = (props.legend && props.legend.length) || 0;
+
+  if (breaks < 1) {
+    return null;
+  }
+
+  const width = 100 / breaks;
+
+  return (
+    <div className="static-legend" style={props.style}>
+      {props.legend.map((item) => {
+        const classes = classNames(
+          'static-legend__item',
+          { 'static-legend__item--invert': item.invert === true }
+        );
+
+        return (
+          <div className={classes} style={{
+            backgroundColor: item.color,
+            width: `${width}%`,
+            maxWidth: `${width}%`
+          }}>{item.label}</div>
+        );
+      })}
+    </div>
+  );
+};
---
+++ src/ClientApp/src/components/StaticLegend/StaticLegend.stories.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import StaticLegend from './StaticLegend';
+
+export default {
+  title: 'StaticLegend'
+};
+
+export const basic = () => (<StaticLegend legend={[{
+  label: 'test',
+  color: '#2ecc40'
+}]} />);
+
+export const noLegend = () => (<StaticLegend />);
+
+export const fourColors = () => (<StaticLegend legend={[{
+  label: 'test',
+  color: '#001f3f'
+}, {
+  label: 'test2',
+  color: '#0074d9'
+}, {
+  label: 'test3',
+  color: '#7fdbff'
+}, {
+  label: 'test4',
+  color: '#39cccc'
+}]} />);
+
+export const textOverflow = () => (<StaticLegend legend={[{
+  label: 'What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  color: '#001f3f'
+}, {
+  label: 'test2',
+  color: '#0074d9'
+}, {
+  label: 'test3',
+  color: '#7fdbff',
+  inverse: true
+}, {
+  label: 'test4',
+  color: '#39cccc'
+}]} />);
+
+export const invertText = () => (<StaticLegend legend={[{
+  label: 'test3',
+  color: '#39cccc',
+  invert: true
+}]} />)
