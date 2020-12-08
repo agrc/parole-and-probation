49d38f77e83b4bd440b0c4b7ae95263d4a782b1a
steveoh <sgourley@utah.gov>
2020-12-08 12:44:01 -0700

    refactor(pallet): replace ftp credentials with api

--- forklift/vault/ftp.template.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# * coding: utf8 *
-'''
-ftp.py
-A module that holds the ftp credentials to get the offender data
-'''
-
-CREDENTIALS = {'host': '', 'username': '', 'password': ''}
