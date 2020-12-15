58b64d8be593d2e249c2a0d34637eba02db6fe32
steveoh <sgourley@utah.gov>
2020-12-14 18:33:46 -0700

    chore(pallet): simplify schema creation
    the new api removes the need for converters

--- forklift/models/schema.py
+++ forklift/models/schema.py
@@ -7,103 +7,84 @@ A module that holds the schema for the dat file
 
 from collections import OrderedDict
 
-import pandas as pd
 from sqlalchemy.dialects.mssql import BIT, CHAR, DATE, DECIMAL, INTEGER, SMALLINT, VARCHAR
 
-
-def convert_to_date(value):
-    return pd.to_datetime(value, errors='coerce')
-
-
-def convert_to_bool(value):
-    if value.lower() == 'y' or value.lower == 't':
-        return 1
-
-    return 0
-
-
-def convert_to_float(value):
-    return convert_to(float, value)
-
-
-def convert_to_int(value):
-    try:
-        value = convert_to(int, value)
-    except Exception:
-        return None
-
-
-def convert_to_str(value):
-    return convert_to(str, value)
-
-
-def convert_to(cast, value):
-    value = value.strip()
-    if value.strip() == '':
-        return None
-
-    return cast(value)
-
-
 type_metadata = OrderedDict([
-    ('legal_status_code', [CHAR(1), convert_to_str]),
-    ('crime_degree', [CHAR(2), convert_to_str]),
-    ('gender', [CHAR(1), convert_to_str]),
-    ('agent_name', [VARCHAR(40), convert_to_str]),
-    ('agent_id', [INTEGER(), convert_to_str]),
-    ('date_of_birth', [DATE(), convert_to_date]),
-    ('supervisor_id', [VARCHAR(8), convert_to_str]),
-    ('supervisor_name', [VARCHAR(35), convert_to_str]),
-    ('offender', [VARCHAR(60), convert_to_str]),
-    ('active_warrant', [BIT(), convert_to_bool]),
-    ('legal_status', [VARCHAR(10), convert_to_str]),
-    ('last_successful_field_contact', [INTEGER(), convert_to_str]),
-    ('supervision_start_date', [DATE(), convert_to_date]),
-    ('offense_description', [VARCHAR(80), convert_to_str]),
-    ('agency', [VARCHAR(30), convert_to_str]),
-    ('race', [VARCHAR(30), convert_to_str]),
-    # ('special_supervision', [VARCHAR(30), convert_to_str]),
-    ('last_attempted_field_contact', [INTEGER(), convert_to_str]),
-    ('last_office_contact', [INTEGER(), convert_to_str]),
-    ('offense_code', [CHAR(1), convert_to_str]),
-    ('primary_offense', [VARCHAR(30), convert_to_str]),
-    ('region', [SMALLINT(), convert_to_str]),
-    ('offender_location', [VARCHAR(40), convert_to_str]),
-    ('offender_id', [INTEGER(), convert_to_str]),
-    ('legal_status_description', [VARCHAR(30), convert_to_str]),
-    ('earned_compliance_credit', [DATE(), convert_to_date]),
-    ('city', [VARCHAR(30), convert_to_str]),
-    ('score', [DECIMAL(5, 2), convert_to_float]),
-    ('address_start_date', [DATE(), convert_to_date]),
-    ('zip', [INTEGER(), convert_to_str]),
-    ('state', [CHAR(2), convert_to_str]),
-    ('address', [VARCHAR(50), convert_to_str]),
-    ('cautions', [VARCHAR(3500), convert_to_str]),
-    ('address_type', [VARCHAR(20), convert_to_str]),
-    ('x', [DECIMAL(24, 20), convert_to_float]),
-    ('y', [DECIMAL(24, 20), convert_to_float]),
-    ('address_type_code', [CHAR(1), convert_to_str]),
-    ('last_field_contact', [DATE(), convert_to_date]),
-    ('field_contact_result', [VARCHAR(20), convert_to_str]),
-    ('alerts', [VARCHAR(500), convert_to_str]),
-    ('unit', [VARCHAR(30), convert_to_str]),
-    ('standard_of_supervision', [VARCHAR(5), convert_to_str]),
-    ('employer', [VARCHAR(30), convert_to_str]),
-    ('employer_phone', [VARCHAR(12), convert_to_str]),
-    ('employer_address', [VARCHAR(60), convert_to_str]),
-    ('offender_phone', [VARCHAR(18), convert_to_str]),
-    ('gang_name', [VARCHAR(30), convert_to_str]),
-    ('gang_type', [VARCHAR(20), convert_to_str]),
-    ('gang_id', [INTEGER(), convert_to_str]),
-
-    # ('gang_type_id', [INTEGER(), convert_to_str]),
-    # ('in_compliance', [BIT(), convert_to_bool]),
-    # ('county', [VARCHAR(12), convert_to_str]),
+    ('legal_status_code', [CHAR(1), 'string']),
+    ('crime_degree', [CHAR(2), 'string']),
+    ('gender', [CHAR(1), 'string']),
+    ('agent_name', [VARCHAR(40), 'string']),
+    ('agent_id', [INTEGER(), 'Int32']),
+    ('date_of_birth', [DATE(), 'datetime64[ns]']),
+    ('supervisor_id', [VARCHAR(8), 'string']),
+    ('supervisor_name', [VARCHAR(35), 'string']),
+    ('offender', [VARCHAR(60), 'string']),
+    ('active_warrant', [BIT(), 'Int8']),
+    ('legal_status', [VARCHAR(10), 'string']),
+    ('last_successful_field_contact', [INTEGER(), 'Int32']),
+    ('supervision_start_date', [DATE(), 'datetime64[ns]']),
+    ('offense_description', [VARCHAR(80), 'string']),
+    ('agency', [VARCHAR(30), 'string']),
+    ('race', [VARCHAR(30), 'string']),
+    ('special_supervision', [VARCHAR(30), 'string']),
+    ('last_attempted_field_contact', [INTEGER(), 'Int32']),
+    ('in_compliance', [BIT(), 'Int8']),
+    ('last_office_contact', [INTEGER(), 'Int32']),
+    ('offense_code', [CHAR(1), 'string']),
+    ('primary_offense', [VARCHAR(30), 'string']),
+    ('region', [SMALLINT(), 'Int8']),
+    ('offender_location', [VARCHAR(40), 'string']),
+    ('offender_id', [INTEGER(), 'Int32']),
+    ('legal_status_description', [VARCHAR(30), 'string']),
+    ('earned_compliance_credit', [DATE(), 'datetime64[ns]']),
+    ('city', [VARCHAR(30), 'string']),
+    ('county', [VARCHAR(12), 'string']),
+    ('score', [DECIMAL(5, 2), 'Int16']),
+    ('address_start_date', [DATE(), 'string']),
+    ('zip', [INTEGER(), 'Int']),
+    ('state', [CHAR(2), 'string']),
+    ('address', [VARCHAR(50), 'string']),
+    ('cautions', [VARCHAR(3500), 'string']),
+    ('address_type', [VARCHAR(20), 'string']),
+    ('x', [DECIMAL(24, 20), 'Int32']),
+    ('y', [DECIMAL(24, 20), 'Int32']),
+    ('address_type_code', [CHAR(1), 'string']),
+    ('last_field_contact', [DATE(), 'datetime64[ns]']),
+    ('field_contact_result', [VARCHAR(20), 'string']),
+    ('alerts', [VARCHAR(500), 'string']),
+    ('unit', [VARCHAR(30), 'string']),
+    ('standard_of_supervision', [VARCHAR(5), 'string']),
+    ('employer', [VARCHAR(30), 'string']),
+    ('employer_phone', [VARCHAR(12), 'string']),
+    ('employer_address', [VARCHAR(60), 'string']),
+    ('offender_phone', [VARCHAR(18), 'string']),
+    ('gang_name', [VARCHAR(30), 'string']),
+    ('gang_type', [VARCHAR(20), 'string']),
+    ('gang_id', [INTEGER(), 'Int32']),
+    ('gang_type_id', [INTEGER(), 'Int32']),
 ])
 
-FIELDS = list(type_metadata.keys())
-FIELDS.append('empty')
+DATA_TYPES = {header: value[1] for (header, value) in type_metadata.items()}
+SQL_TYPES = {header: value[0] for (header, value) in type_metadata.items()}
 
-CONVERTERS = {header: value[1] for (header, value) in type_metadata.items()}
-
-TYPES = {header: value[0] for (header, value) in type_metadata.items()}
+SPECIAL_SUPERVISION = [
+    'CCC',
+    'COMP',
+    'DEP',
+    'DORA',
+    'DRUG CT',
+    'ECR',
+    'EM',
+    'FOSI',
+    'FUG',
+    'GPS',
+    'IG INT',
+    'INCAR',
+    'MIO',
+    'PVP',
+    'RESID',
+    'SO',
+    'SO-A',
+    'SO-B',
+    'SO-C',
+]
