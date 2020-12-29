09d870f152c4d40e0284b055c9b8430719b7b3e2
steveoh <sgourley@utah.gov>
2020-12-29 14:51:59 -0600

    refactor(pallet): update configuration

--- forklift/pallet.py
+++ forklift/pallet.py
@@ -29,9 +29,13 @@ class CorrectionPallet(Pallet):
         self.api = api.ENDPOINT
         self.db = database.CONNECTION
 
-        if configuration != 'Production':
+        if configuration == 'Dev':
             self.api = api.ENDPOINT_AT
             self.db = database.CONNECTION_LOCAL
+
+        if configuration == 'Staging':
+            self.api = api.ENDPOINT_AT
+            self.db = database.CONNECTION_AT
 
     def requires_processing(self):
         if self.dirty is not None:
