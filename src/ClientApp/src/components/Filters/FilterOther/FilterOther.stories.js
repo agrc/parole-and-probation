7b82b963a706db97d242dfdc25e74647b2d0d372
steveoh <sgourley@utah.gov>
2021-01-13 12:32:31 -0700

    test(filter-other): add storybook item

---
+++ src/ClientApp/src/components/Filters/FilterOther/FilterOther.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Component from '../FilterOther';
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  title: 'Filters/Other Filter',
+  component: Component,
+  argTypes: {
+    update: { action: 'update' }
+  }
+};
+
+export const normal = (args) => (
+  <Component criteria={{
+    gang: [{ id: 1, name: 'sureno' }, { id: 2, name: 'crip' }],
+    offense: [{ id: 'A', name: 'murder' }],
+    supervision: [{ id: 1, name: 'ccc' }],
+    warrant: 'Yes',
+    sos: ['low'],
+    status: 'parole'
+  }}
+    update={args.update} />
+);
