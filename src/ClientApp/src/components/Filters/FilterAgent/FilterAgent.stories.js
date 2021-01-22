dfc25fd72ad82107350def88b7cec8909dbe4f61
steveoh <sgourley@utah.gov>
2021-01-21 19:10:51 -0700

    test(app): update filter agent stories

--- src/ClientApp/src/components/Filters/FilterAgent/FilterAgent.stories.js
+++ src/ClientApp/src/components/Filters/FilterAgent/FilterAgent.stories.js
@@ -1,18 +1,69 @@
 import * as React from 'react';
-import { Filters } from '../Filters';
+import { agents, supervisors } from '../lookupData';
+import FilterAgent from './FilterAgent';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
   title: 'Filters/Agent Filter',
-  component: Filters,
+  component: FilterAgent,
   argTypes: {
     dispatch: { action: 'dispatched' }
   }
 };
 
-export const normal = (args) => (
-  <Filters
-    loggedInUser={{ value: 'story tester', id: 999999 }}
-    mapDispatcher={args.dispatch}
+
+let criteria = {
+  agentList: [],
+  supervisorList: [],
+  vanity: false
+}
+
+const reduce = (action) => {
+  let agentList;
+  if (action.payload.add) {
+    agentList = [action.payload.item].concat(criteria.agentList);
+  } else {
+    agentList = criteria.agentList.filter(item => item.value.toLowerCase() !== action.payload.item.value.toLowerCase());
+  }
+
+  criteria = { ...criteria, agentList };
+};
+
+export const empty = (args) => (
+  <FilterAgent
+    data={{ agents, supervisors }}
+    criteria={criteria}
+    update={(action) => {
+      reduce(action);
+      args.dispatch(action);
+    }}
+  />
+);
+
+export const vanity = (args) => (
+  <FilterAgent
+    data={{ agents, supervisors }}
+    criteria={{ ...criteria, vanity: true }}
+    update={(action) => {
+      reduce(action);
+      args.dispatch(action);
+    }}
+  />
+);
+
+export const agentsSelected = (args) => (
+  <FilterAgent
+    data={{ agents, supervisors }}
+    criteria={{
+      ...criteria, agentList: [{
+        id: -1,
+        value: 'Storybook',
+        supervisor: 'Airbnb'
+      }]
+    }}
+    update={(action) => {
+      reduce(action);
+      args.dispatch(action);
+    }}
   />
 );
