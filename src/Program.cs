53e2198703f6755c60460135af6ab7eec2082e39
steveoh <sgourley@utah.gov>
2021-01-21 19:16:54 -0700

    style(api): formatting

--- src/Program.cs
+++ src/Program.cs
@@ -1,60 +1,48 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.GoogleCloudLogging;
 
-namespace parole
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json")
-        .Build();
+namespace parole {
+    public class Program {
+        public static void Main(string[] args) {
+            new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json")
+              .Build();
 
-      var config = new GoogleCloudLoggingSinkOptions
-      {
-        ProjectId = "ut-dts-agrc-parole-dev",
-        LogName = "parole-api",
-        UseSourceContextAsLogName = false,
-        ResourceType = "global",
-        ServiceName = "parole-api",
-        ServiceVersion = "1.0.0",
-        UseJsonOutput = true
-      };
-      config.ResourceLabels.Add("project_id", "ut-dts-agrc-parole-dev");
-      config.Labels.Add("configuration", "staging");
+            var config = new GoogleCloudLoggingSinkOptions {
+                ProjectId = "ut-dts-agrc-parole-dev",
+                LogName = "parole-api",
+                UseSourceContextAsLogName = false,
+                ResourceType = "global",
+                ServiceName = "parole-api",
+                ServiceVersion = "1.0.0",
+                UseJsonOutput = true
+            };
+            config.ResourceLabels.Add("project_id", "ut-dts-agrc-parole-dev");
+            config.Labels.Add("configuration", "staging");
 
-      Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .WriteTo.GoogleCloudLogging(config)
-            .CreateLogger();
+            Log.Logger = new LoggerConfiguration()
+                  .MinimumLevel.Debug()
+                  .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                  .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                  .Enrich.FromLogContext()
+                  .WriteTo.Console()
+                  .WriteTo.GoogleCloudLogging(config)
+                  .CreateLogger();
 
-      Log.Logger.Information("Starting web host...");
+            Log.Logger.Information("Starting web host...");
 
-      CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSerilog()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-  }
 }
