f5fdb68b1297ea726b43af14bec89bd081875eb6
stdavis <stdavis@utah.gov>
2021-01-25 11:48:47 -0700

    refactor(filter): refactor filter components to be more generic

--- src/ClientApp/src/components/Filters/FilterAgent/FilterAgent.js
+++ src/ClientApp/src/components/Filters/FilterAgent/FilterAgent.js
@@ -1,7 +1,7 @@
-import { useCombobox } from 'downshift';
 import { startCase } from 'lodash/string';
 import * as React from 'react';
-import { Button, Card, CardBody, Col, Container, FormGroup, Input, InputGroup, InputGroupAddon, Label } from 'reactstrap';
+import { Button, Card, CardBody, Col, Container, FormGroup, Label } from 'reactstrap';
+import Dropdown from '../../Combobox';
 import '../Filters.css';
 import './FilterAgent.css';
 
@@ -10,9 +10,19 @@ const getAgent = (id, agents) => {
   return agents.filter(item => item.id === id)[0];
 };
 
+const SelectedItems = ({ items, clickHandler }) => (
+  <Card className="mb-3 p-3">
+    <CardBody className="filter-other__items-container p-0">
+      {items.map(item => (
+        <Button className="mb-1" color="secondary" size="sm" outline id={item.id} key={item.id} onClick={clickHandler}>
+          {startCase(item.value.toLowerCase())}
+        </Button>
+      ))}
+    </CardBody>
+  </Card>
+);
 
 export default function FilterAgent(props) {
-
   const updateAgents = (item, add) => {
     console.info('Filters/FilterAgent:updateAgents');
 
@@ -42,178 +52,6 @@ export default function FilterAgent(props) {
     });
   };
 
-  const AgentDropdown = ({ items }) => {
-    const [inputItems, setInputItems] = React.useState(items);
-
-    const {
-      isOpen,
-      getMenuProps,
-      getInputProps,
-      getComboboxProps,
-      highlightedIndex,
-      getItemProps,
-      selectItem,
-      selectedItem
-    } = useCombobox({
-      items: inputItems,
-      defaultHighlightedIndex: 0,
-      itemToString: item => (item ? startCase(item.value.toLowerCase()) : ''),
-      onInputValueChange: ({ inputValue }) => {
-        const filteredItems = items.filter(item =>
-          item.value.toLowerCase().startsWith(inputValue.toLowerCase())
-        );
-
-        if (props.criteria.agentList.length < 1) {
-          return setInputItems(filteredItems);
-        }
-
-        const alreadySelected = props.criteria.agentList.map(x => x.id);
-
-        const difference = filteredItems.filter(x => !alreadySelected.includes(x.id))
-
-        setInputItems(difference);
-      },
-    })
-    return (
-      <div {...getComboboxProps()}>
-        <InputGroup>
-          <Input {...getInputProps({
-            onKeyDown: event => {
-              switch (event.key) {
-                case 'Enter': {
-                  if (selectedItem) {
-                    updateAgents(selectedItem, true);
-                    selectItem(null);
-                  }
-                  break;
-                }
-                default:
-                  break;
-              }
-            }
-          })} />
-          <InputGroupAddon addonType="append">
-            <Button onClick={() => {
-              if (selectedItem) {
-                updateAgents(selectedItem, true);
-                selectItem(null);
-              }
-            }}>Add</Button>
-          </InputGroupAddon>
-        </InputGroup>
-
-        <div className="downshift__match-dropdown" {...getMenuProps({
-          onClick: () => {
-            const selectedItem = inputItems[highlightedIndex];
-            updateAgents(selectedItem, true);
-            selectItem(null);
-          }
-        })}>
-          <ul className="downshift__matches">
-            {isOpen &&
-              inputItems
-                .map((item, index) => (
-                  <li {...getItemProps({
-                    item,
-                    index,
-                    key: item.id,
-                    className: 'downshift__match-item' + (highlightedIndex === index ? ' downshift__match-item--selected' : '')
-                  })}>
-                    {startCase(item.value.toLowerCase())}
-                  </li>
-                ))}
-          </ul>
-        </div>
-      </div>
-    )
-  };
-
-  const SupervisorDropdown = ({ items }) => {
-    const [inputItems, setInputItems] = React.useState(items);
-
-    const {
-      isOpen,
-      getMenuProps,
-      getInputProps,
-      getComboboxProps,
-      highlightedIndex,
-      getItemProps,
-      selectItem,
-      selectedItem
-    } = useCombobox({
-      items: inputItems,
-      defaultHighlightedIndex: 0,
-      itemToString: item => (item ? startCase(item.value.toLowerCase()) : ''),
-      onInputValueChange: ({ inputValue }) => {
-        const filteredItems = items.filter(item =>
-          item.value.toLowerCase().startsWith(inputValue.toLowerCase())
-        );
-
-        if (props.criteria.supervisor === null) {
-          return setInputItems(filteredItems);
-        }
-
-        const alreadySelected = props.criteria.supervisor;
-
-        const difference = filteredItems.filter(x => !alreadySelected.includes(x.value));
-
-        setInputItems(difference);
-      },
-    })
-    return (
-      <div {...getComboboxProps()}>
-        <InputGroup>
-          <Input {...getInputProps({
-            onKeyDown: event => {
-              switch (event.key) {
-                case 'Enter': {
-                  if (selectedItem) {
-                    addAgentsForSupervisor(selectedItem.value);
-                    selectItem(null);
-                  }
-                  break;
-                }
-                default:
-                  break;
-              }
-            }
-          })} />
-          <InputGroupAddon addonType="append">
-            <Button onClick={() => {
-              if (selectedItem) {
-                addAgentsForSupervisor(selectedItem.value);
-                selectItem(null);
-              }
-            }}>Set</Button>
-          </InputGroupAddon>
-        </InputGroup>
-
-        <div className="downshift__match-dropdown" {...getMenuProps({
-          onClick: () => {
-            const selectedItem = inputItems[highlightedIndex];
-            addAgentsForSupervisor(selectedItem.value);
-            selectItem(null);
-          }
-        })}>
-          <ul className="downshift__matches">
-            {isOpen &&
-              inputItems
-                .map((item, index) => (
-                  <li {...getItemProps({
-                    item,
-                    index,
-                    key: item.id,
-                    className: 'downshift__match-item' + (highlightedIndex === index ? ' downshift__match-item--selected' : '')
-                  })}>
-                    {startCase(item.value.toLowerCase())}
-                  </li>
-                ))}
-          </ul>
-        </div>
-      </div>
-    )
-  };
-
   return (
     <Container fluid className="filter-agent">
       <Col>
@@ -227,31 +65,35 @@ export default function FilterAgent(props) {
         </FormGroup>
         <FormGroup>
           <Label>Agent</Label>
-          <AgentDropdown items={props.data.agents}></AgentDropdown>
+          <Dropdown
+            items={props.data.agents}
+            itemPropName="id"
+            isEmpty={props.criteria.agentList.length < 1}
+            alreadySelected={props.criteria.agentList.map(x => x.id)}
+            onSelectItem={item => updateAgents(item, true)}
+            ></Dropdown>
         </FormGroup>
         <FormGroup>
           <Label>Supervisor</Label>
-          <SupervisorDropdown items={props.data.supervisors}></SupervisorDropdown>
+          <Dropdown
+            items={props.data.supervisors}
+            itemPropName="value"
+            isEmpty={props.criteria.supervisor === null}
+            alreadySelected={props.criteria.supervisor}
+            onSelectItem={item => addAgentsForSupervisor(item.value)}
+            ></Dropdown>
         </FormGroup>
       </Col>
       <Col>
         {props.criteria.agentList.length > 0 ? <>
           <Label>Filtering Offenders For</Label>
-          <Card className="mb-3 p-3">
-            <CardBody className="filter-other__items-container p-0">
-              {props.criteria.agentList.map(item => (
-                <Button className="mb-1" color="secondary" size="sm" outline id={item.id} key={item.id} onClick={
-                  event => {
-                    const agent = getAgent(parseInt(event.target.id), props.criteria.agentList);
+          <SelectedItems items={props.criteria.agentList} clickHandler={event => {
+            const agent = getAgent(parseInt(event.target.id), props.criteria.agentList);
 
-                    updateAgents(agent, false)
-                  }}>
-                  {startCase(item.value.toLowerCase())}
-                </Button>
-              ))}
-            </CardBody>
-          </Card>
-        </> : null}
+            updateAgents(agent, false)
+          }} />
+        </>
+        : null }
       </Col>
     </Container>
   )
