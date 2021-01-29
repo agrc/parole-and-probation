5265c0cdb7ef331c59904f2bcaa01fc400dd419c
steveoh <sgourley@utah.gov>
2021-01-28 19:55:38 -0700

    chore(app): add access denied routes

--- src/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.js
+++ src/ClientApp/src/components/api-authorization/ApiAuthorizationRoutes.js
@@ -10,10 +10,20 @@ export default class ApiAuthorizationRoutes extends Component {
         <Route path={ApplicationPaths.Login} render={() => loginAction(LoginActions.Login)} />
         <Route path={ApplicationPaths.LoginFailed} render={() => loginAction(LoginActions.LoginFailed)} />
         <Route path={ApplicationPaths.LoginCallback} render={() => loginAction(LoginActions.LoginCallback)} />
+        <Route path={ApplicationPaths.AccessDenied} render={AccessDenied} />
       </Fragment>
     );
   }
 }
+
+const AccessDenied = () => {
+  return (
+    <>
+      <h1>You do not have access to this resources</h1>
+      <p>If you require access, speak to the Department of Corrections.</p>
+    </>
+  );
+};
 
 function loginAction(name) {
   return <Login action={name}></Login>;
