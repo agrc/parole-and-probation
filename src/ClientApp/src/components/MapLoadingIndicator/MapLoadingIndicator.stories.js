e5c4a722875173ac6c89d3c1ab588023bb888f1c
stdavis <stdavis@utah.gov>
2021-01-29 15:47:00 -0700

    feat(app): implement simple map loading indicator
    Purposefully leaving the styling to a separate commit.
    
    Ref #129

---
+++ src/ClientApp/src/components/MapLoadingIndicator/MapLoadingIndicator.stories.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import HomeButton from './DefaultExtent';
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  title: 'MapTools/DefaultExtent',
+  component: HomeButton,
+  decorators: [(Story) => <div className="esri-ui-top-left esri-ui-corner">{Story()}</div>],
+  argTypes: {
+    goTo: { action: 'goTo' },
+  },
+};
+
+export const Normal = (args) => <HomeButton view={{ goTo: args.goTo, ui: { add: () => {} } }} extent={{}}></HomeButton>;
