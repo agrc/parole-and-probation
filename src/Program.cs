d5b987f48c06a71ff64a0d202733d64ad7600085
steveoh <sgourley@utah.gov>
2021-01-28 19:56:09 -0700

    chore(api): remove cloud logging

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
-using Serilog.Sinks.GoogleCloudLogging;
 
 namespace parole {
     public class Program {
@@ -14,25 +13,12 @@ namespace parole {
               .AddJsonFile("appsettings.json")
               .Build();
 
-            var config = new GoogleCloudLoggingSinkOptions {
-                ProjectId = "ut-dts-agrc-parole-dev",
-                LogName = "parole-api",
-                UseSourceContextAsLogName = false,
-                ResourceType = "global",
-                ServiceName = "parole-api",
-                ServiceVersion = "1.0.0",
-                UseJsonOutput = true
-            };
-            config.ResourceLabels.Add("project_id", "ut-dts-agrc-parole-dev");
-            config.Labels.Add("configuration", "staging");
-
             Log.Logger = new LoggerConfiguration()
                   .MinimumLevel.Debug()
                   .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                   .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                   .Enrich.FromLogContext()
                   .WriteTo.Console()
-                  .WriteTo.GoogleCloudLogging(config)
                   .CreateLogger();
 
             Log.Logger.Information("Starting web host...");
