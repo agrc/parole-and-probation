3c745cb3227d6af51399ad730f6ab40e1db05946
steveoh <sgourley@utah.gov>
2021-02-01 18:15:28 -0700

    fixup! 6cf81f6

--- src/ClientApp/src/components/Filters/FilterOffender/FilterOffender.js
+++ src/ClientApp/src/components/Filters/FilterOffender/FilterOffender.js
@@ -22,137 +22,15 @@ export default function FilterOffender(props) {
     <Container fluid className="filter-offender">
       <form autoComplete="off">
         <Col>
-          <FormGroup>
+          <FormGroup autoComplete="off">
             <Label>Name</Label>
-
             <Dropdown
-              items={props.featureSet}
-              itemToString={(item) => (item ? item.attributes.offender : '')}
-              itemToKey={(item) => (item ? item.attributes.offender_id : '')}
-              type={type}
-              field="OFFENDER_NAME"
-              update={props.update}
+              featureSet={props.items}
+              itemToString={(item) => (item ? item?.attributes?.offender : '')}
+              itemToKey={(item) => (item ? item?.attributes?.offender_id : '')}
+              reducerDescriptor={{ type, field: 'OFFENDER_NAME' }}
+              dispatch={props.update}
             />
-
-            <Dropdown
-              items={props.featureSet}
-              itemToString={(item) => (item ? item.attributes.offender : '')}
-              itemToKey={(item) => (item ? item.attributes.offender_id : '')}
-              onSelectItem={(item) => {
-                props.update({
-                  type,
-                  meta: {
-                    downshift: true,
-                    field: 'OFFENDER_NAME',
-                  },
-                  payload: item.attribute.offender,
-                });
-              }}
-            />
-            <Downshift
-              itemToString={(item) => (item ? item : '')}
-              onChange={(selectedItem) => publish(selectedItem, 'name')}
-              inputValue={props.downshift.offenderName}
-              selectedItem={props.criteria.name}
-              onStateChange={(changes, helpers) => {
-                if (changes.hasOwnProperty('inputValue')) {
-                  props.update({
-                    type,
-                    meta: {
-                      downshift: true,
-                      field: 'OFFENDER_NAME',
-                    },
-                    payload: changes.inputValue,
-                  });
-
-                  if (changes.inputValue.trim() === '') {
-                    helpers.clearSelection();
-                    helpers.closeMenu();
-                  }
-                }
-              }}
-            >
-              {({
-                closeMenu,
-                getInputProps,
-                getItemProps,
-                getMenuProps,
-                highlightedIndex,
-                inputValue,
-                isOpen,
-                setHighlightedIndex,
-                setItemCount,
-              }) => {
-                if (highlightedIndex === null) {
-                  highlightedIndex = 0;
-                }
-                return (
-                  <div>
-                    <Input
-                      {...getInputProps({
-                        onBlur: closeMenu,
-                        onKeyDown: (event) => {
-                          switch (event.key) {
-                            case 'Tab': {
-                              event.preventDefault();
-                              break;
-                            }
-                            default:
-                              break;
-                          }
-                        },
-                        type: 'text',
-                      })}
-                    />
-                    {!isOpen ? null : (
-                      <div className="downshift__match-dropdown" {...getMenuProps()}>
-                        <ul className="downshift__matches">
-                          <FetchItems
-                            field="name"
-                            filter={props.currentFilter}
-                            searchValue={inputValue}
-                            onLoaded={({ data }) => {
-                              console.log('Downshift:onLoaded', data);
-                              if (data) {
-                                setHighlightedIndex(data.length ? 0 : null);
-                                setItemCount(data.length);
-                              }
-                            }}
-                          >
-                            {({ loading, data, error }) => (
-                              <>
-                                {loading ? (
-                                  <li className="downshift__match-item">loading...</li>
-                                ) : error ? (
-                                  <li className="downshift__match-item">error...</li>
-                                ) : data.length ? (
-                                  data.map((item, index) => (
-                                    <li
-                                      {...getItemProps({
-                                        key: item,
-                                        index,
-                                        item,
-                                        className:
-                                          'downshift__match-item' +
-                                          (highlightedIndex === index ? ' downshift__match-item--selected' : ''),
-                                      })}
-                                    >
-                                      {item}
-                                    </li>
-                                  ))
-                                ) : (
-                                  <li className="downshift__match-item">no results...</li>
-                                )}
-                              </>
-                            )}
-                          </FetchItems>
-                        </ul>
-                      </div>
-                    )}
-                  </div>
-                );
-              }}
-            </Downshift>
           </FormGroup>
         </Col>
         <Col>
