2267765784619686cd7cdc840214ff0bad633002
steveoh <sgourley@utah.gov>
2021-02-22 17:42:28 -0700

    test(app): add reset story
    refs #136

---
+++ src/ClientApp/src/components/Filters/FilterActions/FilterActions.stories.js
@@ -0,0 +1,66 @@
+import { Filters } from '../Filters';
+import FilterActions from './FilterActions';
+import './FilterActions.css';
+
+const story = {
+  title: 'Filters/FilterActions',
+  component: FilterActions,
+};
+
+export default story;
+
+export const Normal = () => <FilterActions></FilterActions>;
+
+export const Reset = () => (
+  <Filters
+    initialState={{
+      agent: {
+        loggedInUser: null,
+        agentList: [],
+        supervisor: null,
+        vanity: true,
+      },
+      date: {
+        compliant: 'in',
+        office: 90,
+        attempt: 180,
+        success: 60,
+      },
+      location: {
+        region: [1, 3],
+        zip: '88888',
+        city: 'City',
+        counties: ['County', 'County2'],
+      },
+      offender: {
+        gender: 'Male',
+        name: 'Story book',
+        number: '123123',
+        tel: '123-123-1233',
+        employer: 'Storybook Inc',
+      },
+      other: {
+        warrant: 'Yes',
+        status: 'probation',
+        sos: ['hi', 'mod'],
+        supervision: ['ccc', 'so'],
+        gang: ['crip'],
+        offense: ['A', 'D'],
+      },
+      downshift: {
+        offenderName: '',
+        offenderNumber: '',
+        offenderTelephone: '',
+        offenderEmployer: '',
+      },
+    }}
+    mapDispatcher={() => {}}
+    loggedInUser={{
+      value: 'hi',
+      id: 1,
+    }}
+    visible={true}
+    appliedFilter={''}
+    featureSet={[]}
+  />
+);
