250e8d29e7a20bc046dda25005c370fba36ee047
steveoh <sgourley@utah.gov>
2021-03-11 21:25:44 -0700

    refactor(app): generate service worker with workbox

---
+++ src/ClientApp/build-service-worker.js
@@ -0,0 +1,37 @@
+const workboxBuild = require('workbox-build');
+const getPublicUrlOrPath = require('react-dev-utils/getPublicUrlOrPath');
+const path = require('path');
+const fs = require('fs');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+const publicUrlOrPath = getPublicUrlOrPath(
+  process.env.NODE_ENV === 'development',
+  require(resolveApp('package.json')).homepage,
+  process.env.PUBLIC_URL
+);
+
+workboxBuild
+  .generateSW({
+    swDest: './build/service-worker.js',
+    skipWaiting: true,
+    clientsClaim: true,
+    globDirectory: './build',
+    globPatterns: ['static/**/*.{js,css,png,svg}', 'assets/esri/core/workers/chunks/*.js'],
+    globStrict: true,
+    globIgnores: ['*.LICENSE.txt', '*.map', 'CalciteWebCoreIcons*.svg'],
+    modifyURLPrefix: {
+      assets: `${publicUrlOrPath}assets`,
+      static: `${publicUrlOrPath}static`,
+    },
+    maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+    dontCacheBustURLsMatching: /\.?[0-9a-f]{8}\.?/,
+    sourcemap: false,
+  })
+  .then(({ count, size }) => {
+    console.log(`Generated new service worker ${count} files, totaling ${size} bytes`);
+  })
+  .catch((err) => {
+    console.error(`Unable to generate a new service worker.`, err);
+  });
