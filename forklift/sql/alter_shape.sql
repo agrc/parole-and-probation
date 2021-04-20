a51b356cae1bfc9aaabf5c725076c5f770ad45ac
steveoh <sgourley@utah.gov>
2021-04-19 18:31:26 -0600

    style(pallet): format sql

--- forklift/sql/alter_shape.sql
+++ forklift/sql/alter_shape.sql
@@ -1,6 +1,13 @@
 IF NOT EXISTS (
-  SELECT *
-  FROM sys.columns
-  WHERE object_id = OBJECT_ID('[offenders]') AND name = 'shape'
+  SELECT
+    *
+  FROM
+    sys.columns
+  WHERE
+    object_id = OBJECT_ID('[offenders]')
+    AND name = 'shape'
 )
-  ALTER TABLE [offenders] ADD [shape] geometry
+ALTER TABLE
+  [offenders]
+ADD
+  [shape] geometry
--- forklift/sql/create_primary_key.sql
+++ forklift/sql/create_primary_key.sql
@@ -1,9 +1,17 @@
 IF NOT EXISTS (
-  SELECT *
-  FROM   sys.columns
-  WHERE  object_id = OBJECT_ID('[offenders]') AND name = 'id'
+  SELECT
+    *
+  FROM
+    sys.columns
+  WHERE
+    object_id = OBJECT_ID('[offenders]')
+    AND name = 'id'
 )
-  ALTER TABLE [offenders] ADD [id] int NOT NULL IDENTITY (1,1)
-
-  ALTER TABLE [offenders]
-  ADD CONSTRAINT [PK_id] primary key(id);
+ALTER TABLE
+  [offenders]
+ADD
+  [id] int NOT NULL IDENTITY (1, 1)
+ALTER TABLE
+  [offenders]
+ADD
+  CONSTRAINT [PK_id] primary key(id);
