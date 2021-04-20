1a6d2feefb33911eaeeafec9552f131d13d57d16
steveoh <sgourley@utah.gov>
2021-04-20 11:35:57 -0600

    refactor(api): configuration does not work with immutable types

--- src/Features/Email/EmailConfig.cs
+++ src/Features/Email/EmailConfig.cs
@@ -1,9 +1,7 @@
 namespace parole.Features {
-    // public class EmailConfig {
-    //     public string Smtp { get; set; }
-    //     public string Pickup { get; set; }
-    //     public bool Testing { get; set; }
-    // }
-
-    public record EmailConfig(string Smtp, string Pickup, bool Testing);
+    public class EmailConfig {
+        public string Smtp { get; set; } = default!;
+        public string Pickup { get; set; } = default!;
+        public bool Testing { get; set; }
+    }
 }
