0f588c408f9c499d8d1da2abb897bacf038673c0
steveoh <sgourley@utah.gov>
2023-04-03 15:13:16 -0600

    chore: switch to podman

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,9 +1,9 @@
 set -e
 cd "$(dirname "$0")/.."
 
-docker compose down
+podman-compose down
 echo "starting databases"
-docker compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach cache
+podman-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach cache
 
 echo "starting api"
 cd src
