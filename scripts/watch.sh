e18c0763296bb3720cb2708cb16bdc63d47583a5
steveoh <sgourley@utah.gov>
2024-06-12 16:21:48 -0600

    chore: revert back to rancher/docker

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,9 +1,9 @@
 set -e
 cd "$(dirname "$0")/.."
 
-podman-compose down
+docker-compose down
 echo "starting databases"
-podman-compose --file docker-compose.yaml --file docker-compose.override.yaml up --detach cache
+docker-compose --file docker-compose.yaml up --detach cache
 
 echo "starting api"
 cd src
