120942fc99732d42badac79d359ade86a929cc3d
Steve Gourley <sgourley@utah.gov>
2025-11-06 20:53:01 -0700

    chore(forklift): fix bulk sql loading

--- forklift/vault/api.template.py
+++ forklift/vault/api.template.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # * coding: utf8 *
-'''
+"""
 api.py
 A module that holds the api credentials to get the offender data
-'''
+"""
 
-AUTHORIZATION_HEADER = {'Authorization': 'Apikey 0000'}
-ENDPOINT_AT = 'qa url'
-ENDPOINT = 'production url'
+AUTHORIZATION_HEADER = {"Authorization": "Apikey 0000"}
+ENDPOINT_AT = "qa url"
+ENDPOINT = "production url"
