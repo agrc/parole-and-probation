b72cc77b70f79c1deae148559d81a285633f812b
steveoh <sgourley@utah.gov>
2025-11-10 18:26:06 -0700

    chore: improve local debugging

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -7,4 +7,10 @@ docker-compose --file docker-compose.yaml up --detach cache
 
 echo "starting api"
 cd src
-dotnet watch
+
+export NODE_ENV=development
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_HOSTINGSTARTUPASSEMBLIES=Microsoft.AspNetCore.SpaProxy
+export ASPNETCORE_URLS="https://localhost:5001;http://localhost:5000"
+
+dotnet watch --launch-profile "app"
